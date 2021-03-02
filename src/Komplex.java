7a9038c4d5414ad85971a3aa8b144ce21638d99a
Segerkung <60388987+Segerkung@users.noreply.github.com>
2021-03-02 11:14:01 +0100

    komplex tal

---
+++ src/Komplex.java
@@ -0,0 +1,29 @@
+public class Komplex {
+
+
+    public double re;
+    public double im;
+    public Komplex(double r, double i ){
+
+        this.re = r;
+        this.im = i;
+    }
+
+    @Override
+    public String toString() {
+        return "Komplex{" +
+                "re=" + re +
+                ", im=" + im +
+                '}';
+    }
+
+    public Komplex add(Komplex a){
+    }
+
+    public Komplex multiply(Komplex a){
+
+    }
+
+
+
+}
