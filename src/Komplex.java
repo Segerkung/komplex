b34ca421ec987cd6233a8fef0adde0cf06cfa80c
Segerkung <60388987+Segerkung@users.noreply.github.com>
2021-03-05 11:13:09 +0100

    Lade till beräkning och ska klura vidare hur man ska göra med beräkningen på multiplikation. Sedan har jag börjat på min arg

--- src/Komplex.java
+++ src/Komplex.java
@@ -3,27 +3,38 @@ public class Komplex {
 
     public double re;
     public double im;
-    public Komplex(double r, double i ){
+
+    public Komplex(double r, double i) {
 
         this.re = r;
         this.im = i;
     }
 
     @Override
-    public String toString() {
+    public String toString() {                      // skriver talet
         return "Komplex{" +
                 "re=" + re +
                 ", im=" + im +
                 '}';
     }
 
-    public Komplex add(Komplex a){
+    public Komplex add(Komplex out) {                //Beräkning
+        double real = this.re + out.re;
+        double img = this.im + out.im;
+        Komplex svar = new Komplex(real, img);
+        return svar;
+
     }
 
-    public Komplex multiply(Komplex a){
+    public Komplex multiply(Komplex out) {           //Beräkning
+        double real = this.re * out.re;
+        double img = this.im * out.im;
+        Komplex svar = new Komplex(real, img);
+        return svar;
 
     }
 
-
-
+    public double arg() {
+        return 0;
+    }
 }
